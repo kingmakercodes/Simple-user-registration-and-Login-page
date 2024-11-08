a5847c1a16ee1abcb2255bc3ee68ddbac1d39d5a
Marc Banson <bansonmarc@gmail.com>
2024-11-08 14:39:03 +0000

    -added login route to user authentication

--- back-end/app/routes/auth.py
+++ back-end/app/routes/auth.py
@@ -1,6 +1,10 @@
+import os
+from datetime import datetime, timezone, timedelta
+
 from flask import Blueprint, request, jsonify
 from app import database
 from app.models.models import User
+import jwt
 
 auth_blueprint= Blueprint('auth', __name__)
 
@@ -12,6 +16,7 @@ def home():
 @auth_blueprint.route('/signup', methods=['POST'])
 def register():
     try:
+        # collect user data from forms
         data= request.get_json()
         fullname= data.get('fullname')
         email= data.get('email')
@@ -33,3 +38,35 @@ def register():
     except Exception as e:
         print(f'Error: {e}')
         return jsonify({'error':'An internal error occurred.'}), 500
+
+
+# user login route
+@auth_blueprint.route('/login', methods=['POST'])
+def login():
+    try:
+        # collect client-validated user login details, empty fields to be handled client-side
+        data= request.get_json()
+        email= data.get('email')
+        password= data.get('password')
+
+        user= User.query.filter_by(email=email).first()
+
+        if not user:
+            return jsonify({'message':'Invalid email!'}), 401
+        if password!= user.password_hash:
+            return jsonify({'message':'Password incorrect!'}), 401
+
+        # generate token for user login session
+        payload= {
+            'user_id': user.id,
+            'exp': datetime.now(timezone.utc)+ timedelta(hours=3)
+        }
+        token= jwt.encode(payload, os.getenv('JWT_SECRET_KEY'), algorithm='HS256')
+
+        return jsonify({
+                'token':token,
+                'message':'User logged in successfully!',
+        }), 201
+
+    except Exception as e:
+        return jsonify({'error':f'An internal error occurred! {e}'})
