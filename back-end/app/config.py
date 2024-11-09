66a0f857b45659c8f59c327ce496cb758542a101
Marc Banson <bansonmarc@gmail.com>
2024-11-09 09:23:26 +0000

    added: header name and type as I kept getting errors when testing route

--- back-end/app/config.py
+++ back-end/app/config.py
@@ -7,6 +7,9 @@ load_dotenv()
 class Config:
     SECRET_KEY= os.getenv('SECRET_KEY', 'default_secret_key')
     JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY', 'jwt_secret_key')
+    JWT_TOKEN_LOCATION= os.getenv('JWT_TOKEN_LOCATION', 'headers').split(',')
+    JWT_HEADER_NAME='Authorization'
+    JWT_HEADER_TYPE='Bearer'
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS= False
     CORS_HEADERS='Content-Type'
