15e083eae886be98e66eed793378dec6b37439cc
Marc Banson <bansonmarc@gmail.com>
2024-11-12 17:30:59 +0000

    -added: mail configurations to send verification emails

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from app.config import DevelopmentConfig
+from flask_mail import Mail
 
 # define global instances
 database= SQLAlchemy()
+mail= Mail()
 
 # function to create app
 def create_app():
@@ -16,6 +18,7 @@ def create_app():
     # initialize global instances
     database.init_app(app)
     CORS(app, resources={r"/*":{"origins": "http://localhost:63343"}}, supports_credentials=True) # CORS configurations to allow use of credentials for using cookies in session
+    mail.init_app(app)
 
     # register blueprints
     from app.routes.auth import auth_blueprint
