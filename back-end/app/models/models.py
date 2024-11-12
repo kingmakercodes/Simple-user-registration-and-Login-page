2edaa9972331d8a369bf28e9eca275a874e1ffc7
Marc Banson <bansonmarc@gmail.com>
2024-11-12 17:41:37 +0000

    -added: BaseUser model as abstract to User and PendingUser models -added: PendingUser model to serve as temporary storage for User model details until email is verified -modified: User model to inherit from BaseUser model and hash passwords only if User model is being committed

--- back-end/app/models/models.py
+++ back-end/app/models/models.py
@@ -1,16 +1,34 @@
+from datetime import datetime, timezone
 from app import database
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(database.Model):
-    __tablename__='users'
+# base model for user models and pending user models inheritances
+class BaseUser(database.Model):
+    __abstract__= True # prevents this model from becoming a database table
 
     id= database.Column(database.Integer, primary_key=True)
     fullname= database.Column(database.String(60), unique=True, nullable=False)
     email= database.Column(database.String(120), unique=True, nullable=False)
-    password_hash= database.Column(database.String(128), nullable=False)
+    created_at= database.Column(database.DateTime, default=datetime.now(timezone.utc))
 
-    # possible relationships and foreign keys to be defined later
+
+# pending user model
+class PendingUser(BaseUser):
+    __tablename__='pending_users'
+
+    # additional fields specific to pending users
+    token= database.Column(database.String(500), nullable=False)
+    password= database.Column(database.String(20), nullable=False)
+
+
+# registered user model
+class User(BaseUser):
+    __tablename__='users'
+
+    # additional fields specific to verified registered users
+    is_verified= database.Column(database.Boolean, default=False)
+    password_hash= database.Column(database.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash= generate_password_hash(password)
